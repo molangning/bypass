262190e65f2d0f7d06b3a802528f13f21662c724
Mo Langning <133737702+molangning@users.noreply.github.com>
2023-08-27 17:11:57 +0800

    Update common_functions.php

--- common_functions.php
+++ common_functions.php
@@ -5,19 +5,18 @@
     $bytes = random_bytes(16);
     $uuid = "uuid_".bin2hex($bytes);
     setcookie("uuid",$uuid);
+    header("Refresh:0");
   }
 
+  if (isset($_COOKIE["uuid"])){
+    $uuid=$_COOKIE["uuid"];
 
-
-  if (!isset($uuid)){
+    if (strlen($uuid) !== 37){
+      set_uuid();
+    } else if (!str_starts_with($uuid, "uuid_")){
     set_uuid();
-  }
-
-  $uuid=$_COOKIE["uuid"];
-
-  if (strlen($uuid) !== 37){
-    set_uuid();
-  } else if (!str_starts_with($uuid, "uuid_")){
+    }
+  } else {
     set_uuid();
   }
 
