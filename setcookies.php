011ddeb55f4d2badc62d24cb37b823cb1a644821
molangning <133737702+molangning@users.noreply.github.com>
2023-10-20 10:41:31 +0000

    a MORE permanent cookie fix

--- setcookies.php
+++ setcookies.php
@@ -1,8 +1,4 @@
 <!DOCTYPE html>
 <html lang="en-GB">
-  <script>
-    var timezone = Intl.DateTimeFormat().resolvedOptions().timeZone
-    document.cookie = "timezone=" + timezone + ";path=/";
-    history.go(-1);
-  </script>
+  <script>var timezone=Intl.DateTimeFormat().resolvedOptions().timeZone;document.cookie="timezone="+timezone+";path=/";</script>
 </html>
