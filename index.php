c36608c981e841e068e0da9689faca0b6561b903
molangning <133737702+molangning@users.noreply.github.com>
2023-10-23 02:39:13 +0000

    permalink support final?

--- index.php
+++ index.php
@@ -1,28 +1,27 @@
 <?php
-
 $request = $_SERVER['REQUEST_URI'];
-$viewDir = '/views/';
 
 switch ($request) {
-    case '':
-    case '/':
-        require __DIR__ . '/fake.php';
-        break;
-
-    case '/bypass':
-        require __DIR__ . '/bypass.php';
-        break;
-  
-    case '/debug':
-        require __DIR__ . '/debug.php';
-        break;
+  case '':
+  case '/':
+    require __DIR__ . '/fake.php';
+    break;
+  case '/bypass':
+    require __DIR__ . '/bypass.php';
+    break;
+  case '/debug':
+    require __DIR__ . '/debug.php';
+    break;
   case '/stage-1.php':
-        require __DIR__ . '/stage-1.php';
-        break;
+    require __DIR__ . '/stage-1.php';
+    break;
   case '/setcookies.php':
-        require __DIR__ . '/setcookies.php';
-        break;
+    require __DIR__ . '/setcookies.php';
+    break;
+  case '/permalink':
+    require __DIR__ . '/permalink.php';
+    break;
   default:
-        require __DIR__. '/error.php';
+    require __DIR__. '/error.php';
 }
 ?>
